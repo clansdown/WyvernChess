eb73ad1fed95e7c245a01fb11ec7283dc78a43da
Christopher Lansdown <chris@chrislansdown.com>
2023-07-23 16:38:24 -0400

    setup

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,47 +1,38 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+
+
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<h2>WyvernChess</h2>
 
-  <div class="card">
-    <Counter />
-  </div>
+<canvas id="board" width=512 height=512></canvas>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
+main {
+     height: 100%;
+}
+
+canvas#board {
+    border: 1px solid #000;
+    width: 100%;
+    aspect-ratio: 1/1;
+    display: block;
+}
+
+@media (min-aspect-ratio: 1/1) {
+       canvas#board {
+                    width: auto;
+                    height: 90%;
+           
+           aspect-ratio: 1/ 1;
+           border: 1px solid #f00;
+       }
+}
+
 </style>
